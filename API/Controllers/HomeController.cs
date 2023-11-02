7fb87d9ffe9604dd20fd6c3b9e625b9b3e970ff7
Lê Quang Tùng <lequangtung2002@gmail.com>
2023-11-02 17:31:43 +0700

    test controller

---
+++ API/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// Controller for publishing 
+    /// </summary>
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public HomeController()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("eCardLearn");
+        }
+    }
+}
