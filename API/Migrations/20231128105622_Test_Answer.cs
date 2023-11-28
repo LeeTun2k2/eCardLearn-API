257944577af2387cffa883ad66a0ec9563a639e6
Lê Quang Tùng <lequangtung2002@gmail.com>
2023-11-28 18:00:57 +0700

    Test answer fk

---
+++ API/Migrations/20231128105622_Test_Answer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test_Answer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestAnswer_AspNetUsers_TestAnswerId",
+                table: "TestAnswer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestAnswer_StudentId",
+                table: "TestAnswer",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestAnswer_AspNetUsers_StudentId",
+                table: "TestAnswer",
+                column: "StudentId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestAnswer_AspNetUsers_StudentId",
+                table: "TestAnswer");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestAnswer_StudentId",
+                table: "TestAnswer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestAnswer_AspNetUsers_TestAnswerId",
+                table: "TestAnswer",
+                column: "TestAnswerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
