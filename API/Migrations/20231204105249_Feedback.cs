a15e078a2e8b7a1b696d99ac91c1d040dc706ce0
Lê Quang Tùng <lequangtung2002@gmail.com>
2023-12-04 17:53:31 +0700

    FK feedback

---
+++ API/Migrations/20231204105249_Feedback.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Feedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Feedback_AspNetUsers_FeedbackId",
+                table: "Feedback");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedback_UserId",
+                table: "Feedback",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Feedback_AspNetUsers_UserId",
+                table: "Feedback",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Feedback_AspNetUsers_UserId",
+                table: "Feedback");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Feedback_UserId",
+                table: "Feedback");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Feedback_AspNetUsers_FeedbackId",
+                table: "Feedback",
+                column: "FeedbackId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
