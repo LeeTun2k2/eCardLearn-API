c0eeb3f37f97750685d092c213a332008185d8df
Lê Quang Tùng <lequangtung2002@gmail.com>
2023-12-07 16:10:10 +0700

    TestReport

---
+++ API/Data/DTOs/TestResult/Summary.cs
@@ -0,0 +1,36 @@
+﻿using API.Data.DTOs.Authentication;
+using API.Data.DTOs.Class;
+
+namespace API.Data.DTOs.TestResult
+{
+    /// <summary>
+    /// TestResult 
+    /// </summary>
+    public class TestResult_Summary
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public Guid SummaryId { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// User Id
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// Fullname
+        /// </summary>
+        public string FullName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Số câu đúng
+        /// </summary>
+        public int NoCorrectAnswer { get; set; } = 0;
+
+        /// <summary>
+        /// Số câu sai
+        /// </summary>
+        public int NoIncorrectAnswer { get; set; } = 0;
+    }
+}
